4f400ae40b9bb2b82cebc40e2a0bb9d292c78d1f
Ricardo Jesus Silva <ricardo.jesus.silva@novabase.pt>
2014-02-02 02:49:17 +0000

    animies are now spawned and get closed to the player

--- Assets/Scripts/Movement/EnemyMoveAround.cs
+++ Assets/Scripts/Movement/EnemyMoveAround.cs
@@ -8,28 +8,22 @@ public class EnemyMoveAround : MonoBehaviour {
 	public float originDistance;
 	public int numberOfEnemies;
 
-	private Hashtable ht = new Hashtable();
-	private GameObject nextEnemie;
+
+	private int currentEnemies;
+	private Hashtable ht;
+	private GameObject nextEnemy;
 
 	void Awake(){
 
 	}
 
-	// Use this for initialization
+	// we have 0 enemies displaying
+	// set the enemy position
+	// spawn a enemy
 	void Start () {
-		Vector3 topRight = new Vector3 (originDistance, originDistance, 0);
-		Vector3 topLeft = new Vector3 (-originDistance, originDistance, 0);
-		Vector3 botLeft = new Vector3 (-originDistance, -originDistance, 0);
-		Vector3 botRight = new Vector3 (originDistance, -originDistance, 0);
-		Vector3[] path = {topRight, topLeft, botLeft, botRight, topRight};
-		
-		ht.Add("path",path);
-		ht.Add("time",moveSpeed);
-		ht.Add("delay",1);
-		ht.Add("looptype",iTween.LoopType.none);
-		ht.Add("onstart", "CanSendNewEnemie");
-		ht.Add ("onstarttarget", this.gameObject);
-		this.spawnEnemie ();
+		currentEnemies = 0;
+		this.setEnemyPath ();
+		this.spawnEnemy ();
 	}
 	
 	// Update is called once per frame
@@ -37,20 +31,49 @@ public class EnemyMoveAround : MonoBehaviour {
 
 	}
 
-	void spawnEnemie () {
-		Debug.Log("spawning");
-		nextEnemie = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
-		nextEnemie.transform.position = new Vector3(originDistance, originDistance, 0);
-		iTween.MoveTo (nextEnemie, ht);
+	// based on the origin position creates a new enemy path
+	void setEnemyPath () {
+		transform.position = new Vector3 (originDistance, originDistance, 0);
+		Vector3 topRight = new Vector3 (originDistance, originDistance, 0);
+		Vector3 topLeft = new Vector3 (-originDistance, originDistance, 0);
+		Vector3 botLeft = new Vector3 (-originDistance, -originDistance, 0);
+		Vector3 botRight = new Vector3 (originDistance, -originDistance, 0);
+		Vector3[] path = {topRight, topLeft, botLeft, botRight, topRight};
+
+		ht = new Hashtable();
+		ht.Add("path",path);
+		ht.Add("time",moveSpeed);
+		ht.Add("delay",1);
+		ht.Add("looptype",iTween.LoopType.none);
+		ht.Add("onstart", "CanSendNextEnemy");
+		ht.Add ("onstarttarget", this.gameObject);
+	}
+		
+	// spawns a new anemie
+	void spawnEnemy () {
+		nextEnemy = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+		nextEnemy.tag = "Enemy";
+		nextEnemy.transform.position = new Vector3(originDistance, originDistance, 0);
+		iTween.MoveTo (nextEnemy, ht);
 	}
 
-	void CanSendNewEnemie () {
-		if (numberOfEnemies == 0) {
-			return;
+	// called after an enemy completes its path
+	// if the enemy limit was not reached a new enemy is spawed
+	// otherwise if the originDistace is not to near from the player seends another wave of enemies
+	// when a new wave is created 
+
+	void CanSendNextEnemy () {
+		if (numberOfEnemies == currentEnemies && originDistance > 2) {
+			originDistance -= 1.0f;
+			GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+			foreach (GameObject obj in allEnemies) {
+				DestroyObject(obj);
+			}
+			this.Start();
+		} else if (currentEnemies < numberOfEnemies) {
+			currentEnemies++;
+			this.spawnEnemy ();
 		}
-		Debug.Log("can send new enemies");
-		numberOfEnemies--;
-		this.spawnEnemie ();
-
 	}
+
 }
